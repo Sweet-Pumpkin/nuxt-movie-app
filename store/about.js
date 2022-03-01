8bdf8e45e2dc1ca909b99ebf0f42d471d427c969
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-03-02 03:59:29 +0900

    Migration to Nuxt

---
+++ store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'SWEET PUMPKIN',
+    email: 'sweetpumpkin22@naver.com',
+    blog: 'https://github.com/Sweet-Pumpkin',
+    phone: '+82-10-1234-5678',
+    image: 'https://infallible-hopper-7b26f5.netlify.app/a152c2b87dfd2224e788a0c9997b8393.png'
+  })
+}
